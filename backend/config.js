0d2ad628660b9533acd1cb4036586ca99e08b715
urasil <114501016+urasil@users.noreply.github.com>
2023-09-26 12:46:01 +0100

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555
 
-export const mongoDBUrl = "mongodb+srv://root:root@bookstore.8wo9zwq.mongodb.net/?retryWrites=true&w=majority";
+export const mongoDBUrl = "mongodb+srv://username:password@bookstore.8wo9zwq.mongodb.net/?retryWrites=true&w=majority";
